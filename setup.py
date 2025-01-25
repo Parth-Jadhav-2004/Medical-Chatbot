b116a64b03c507269c9a20562831e4ee06aed2d6
Parth Jadhav <jadhavparth2004@gmail.com>
2025-01-25 11:00:01 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Parth Jadhav',
+    author_email= 'jadhavparth2004@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
